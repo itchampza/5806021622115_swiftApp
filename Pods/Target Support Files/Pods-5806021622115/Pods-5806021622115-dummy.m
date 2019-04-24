d419cd23b0e6d8ec0cf7c07e5db47304f240535d
itchampza <champ.r0138@gmail.com>
2019-04-24 15:38:08 +0700

    success code

---
+++ Pods/Target Support Files/Pods-5806021622115/Pods-5806021622115-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_5806021622115 : NSObject
+@end
+@implementation PodsDummy_Pods_5806021622115
+@end
---
+++ Pods/Target Support Files/Pods-5806021622115Tests/Pods-5806021622115Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_5806021622115Tests : NSObject
+@end
+@implementation PodsDummy_Pods_5806021622115Tests
+@end
